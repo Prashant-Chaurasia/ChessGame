02620d9fb6f7d2d17f96f4b0abf1dbeaab6ec941
Prashant Chaurasia <prashant.chaurasia1311@gmail.com>
2016-12-02 18:56:47 +0530

    Modified the classes

--- src/ChessBoard/Board.java
+++ src/ChessBoard/Board.java
@@ -5,16 +5,27 @@ import PIECES.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Board extends JFrame {
+    JButton pieceToMoveButton = null;//variable that persists between actionPerformed calls
+    String moveCheck = "black";
+    Piece tempObject ;
+    static int crrtRow = -1, crrtCol = -1;
     private float sizeX = (float) 80.0, sizeY = (float) 80.0;
-    private float posX = (float) 0.0, posY = (float) 0.0;
     private Color boxColor = Color.BLACK;
     private JPanel pane;
     private JButton board[][] = new JButton[8][8];
     GridLayout boardLayout = new GridLayout(8, 8);
     Player black;
     Player white;
+    int turn = 0 ;
+    int turn1 = 1;
+
+    public Board(){
+
+    }
 
     public Board(Player b, Player w) {
         black = b;
@@ -29,18 +40,21 @@ public class Board extends JFrame {
         GridLayout boardLayout = new GridLayout(8, 8);
         setLayout(boardLayout);
         setTitle("Chess Game");
-        setBounds(0, 0, 660, 660);
+        //pane = new JPanel();
+        setBounds(360, 0, 660, 660);
+        //pane.setSize(640,640);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 setBoxColor((i + j) % 2);
                 if (board[i][j] != null) {
+                    board[i][j].setSize(new Dimension(80, 80));
+                    board[i][j].addActionListener(new MoveListener());
                     board[i][j].setBackground(boxColor);
                     this.getContentPane().add(board[i][j], boardLayout);
-
                 } else {
                     board[i][j] = new JButton();
+                    board[i][j].addActionListener(new MoveListener());
                     board[i][j].setSize(new Dimension(80, 80));
                     board[i][j].setBackground(boxColor);
                     this.getContentPane().add(board[i][j], boardLayout);
@@ -48,55 +62,56 @@ public class Board extends JFrame {
                 }
             }
         }
-        //add(pane);
         setVisible(true);
-
     }
 
     protected void setPieces(char ch, Player p) {
         int i;
-        String s = "";
+        String s ;
         if (ch == 'W') {
             i = 0;
+            s = "white";
         } else {
             i = 6;
+            s = "black";
         }
         int count = 0;
         while (count < 2) {
-            for (int j = 0; j < 8; j++) {
-                if (i == 1) {
-                    p.pawn[j] = new Pawn("Photos//" + ch + "P.gif");
-                    board[i][j] = p.pawn[j];
-                    board[i][j].setSize(new Dimension(80, 80));
-
-                } else if (i == 6) {
-                    p.pawn[j] = new Pawn("Photos//" + ch + "P.gif");
-                    board[i][j] = p.pawn[j];
-                    board[i][j].setSize(new Dimension(80, 80));
-                } else {
-                    if (j == 0 || j == 7) {
-                        p.rook[count] = new Rook("Photos//" + ch + "R.gif");
-                        board[i][j] = p.rook[count];
+            if(i==1||i==6) {
+                for (int j = 0; j < 8; j++) {
+                    if (i == 1) {
+                        p.pawn[j] = new Pawn(white, black, board, "Photos//" + ch + "P.gif", s);
+                        board[i][j] = p.pawn[j];
                         board[i][j].setSize(new Dimension(80, 80));
-                    } else if (j == 1 || j == 6) {
-                        p.knight[count] = new Knight("Photos//" + ch + "N.gif");
-                        board[i][j] = p.knight[count];
-                        board[i][j].setSize(new Dimension(80, 80));
-                    } else if (j == 2 || j == 5) {
-                        p.bishop[count] = new Bishop("Photos//" + ch + "B.gif");
-                        board[i][j] = p.bishop[count];
-
-                        board[i][j].setSize(new Dimension(80, 80));
-                    } else if (j == 3) {
-                        p.king = new King("Photos//" + ch + "Q.gif");
-                        board[i][j] = p.king;
-                        board[i][j].setSize(new Dimension(80, 80));
-                    } else if (j == 4) {
-                        p.queen = new Queen("Photos//" + ch + "K.gif");
-                        board[i][j] = p.queen;
+                    } else if (i == 6) {
+                        p.pawn[j] = new Pawn(white, black, board, "Photos//" + ch + "P.gif", s);
+                        board[i][j] = p.pawn[j];
                         board[i][j].setSize(new Dimension(80, 80));
                     }
                 }
+            }
+            else{
+                p.rook[0] = new Rook(white,black,board,"Photos//" + ch + "R.gif", s);
+                p.rook[1] = new Rook(white,black,board,"Photos//" + ch + "R.gif", s);
+
+                p.knight[0] = new Knight(white,black,board,"Photos//" + ch + "N.gif", s);
+                p.knight[1] = new Knight(white,black,board,"Photos//" + ch + "N.gif", s);
+
+                p.bishop[0] = new Bishop(white,black,board,"Photos//" + ch + "B.gif", s);
+                p.bishop[1] = new Bishop(white,black,board,"Photos//" + ch + "B.gif", s);
+
+                p.king = new King(white,black,board,"Photos//" + ch + "K.gif", s);
+                p.queen = new Queen(white,black,board,"Photos//" + ch + "Q.gif", s);
+
+                board[i][0] = p.rook[0];
+                board[i][7] = p.rook[1];
+                board[i][1] = p.knight[0];
+                board[i][6] = p.knight[1];
+                board[i][2] = p.bishop[0];
+                board[i][5] = p.bishop[1];
+                board[i][3] = p.king;
+                board[i][4] = p.queen;
+
             }
             count++;
             i++;
@@ -111,5 +126,110 @@ public class Board extends JFrame {
             boxColor = Color.WHITE;
 
     }
+    public Player getWhitePlayer(){
+        return white;
+    }
+
+    public Player getBlackPlayer(){
+        return black;
+    }
+
+
+
+    class MoveListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+
+           /* for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    if (board[row][col] == (JButton)actionEvent.getSource()) {
+                        if (pieceToMoveButton == null&&tempObject==null){
+                            for(int i=0;i<8;i++){
+                                if(board[row][col]==white.pawn[i]) {
+                                    tempObject = white.pawn[i];
+                                    break;
+                                }
+                                if(board[row][col]==black.pawn[i]) {
+                                    tempObject = black.pawn[i];
+                                    break;
+                                }
+                            }
+                            for(int i = 0 ;i<2 ;i++){
+                                if(board[row][col] == white.rook[i]) {
+                                    tempObject = white.rook[i];
+                                    break;
+                                }
+                                if(board[row][col] == black.rook[i]) {
+                                    tempObject = black.rook[i];
+                                    break;
+                                }
+                                if(board[row][col] == white.bishop[i]) {
+                                    tempObject = white.bishop[i];
+                                    break;
+                                }
+                                if(board[row][col] == black.bishop[i]) {
+                                    tempObject = black.bishop[i];
+                                    break;
+                                }
+                                if(board[row][col] == white.knight[i]) {
+                                    tempObject = white.knight[i];
+                                }
+                                if(board[row][col] == black.knight[i]) {
+                                    tempObject = black.knight[i];
+                                    break;
+                                }
+                            }
+                            if(board[row][col] == white.king)
+                                tempObject = white.king;
+                            if(board[row][col] == black.king)
+                                tempObject = black.king;
+                            if(board[row][col] == white.queen)
+                                tempObject = white.queen;
+                            if(board[row][col] == black.queen)
+                                tempObject = black.queen;
+                            // System.out.println("row" + row + "col" + col);
+
+                            if(tempObject!=null){
+                                pieceToMoveButton = tempObject;
+                                board[row][col] = null;
+                                crrtRow = row;
+                                crrtCol = col;
+                                turn = tempObject.getColor()=="black"?0:1;
+                                //System.out.println(turn);
+                            }
+                            //System.out.println(pieceToMoveButton);
+                            break;
+                        }
+                        //if this button press is selecting where to move
+                        else {
+                            //System.out.print("   Hello");
+                            //System.out.println(tempObject.validateMove(board,crrtRow,crrtCol,row,col)+ "   " + tempObject.getColor()+" ");
+                            if(tempObject.validateMove(crrtRow,crrtCol,row,col)&&turn == turn1){
+                                board[row][col] = tempObject;
+                                turn1++;
+                                turn1=turn1%2;
+                                getContentPane().removeAll();
+                                createBoard();
+                                pieceToMoveButton = null;    //makes the next button press a piece selection
+                                tempObject = null;
+                                crrtRow = -1;
+                                crrtCol = -1;
+                            }
+                            else{
+                                board[crrtRow][crrtCol] = pieceToMoveButton;
+                                getContentPane().removeAll();
+                                createBoard();
+                                pieceToMoveButton = null;    //makes the next button press a piece selection
+                                tempObject = null;
+                                crrtRow = -1;
+                                crrtCol = -1;
+                            }
+                        }
+                    }
+                }
+            }*/
+        }
+    }
+
 }
 
--- src/PIECES/Piece.java
+++ src/PIECES/Piece.java
@@ -1,18 +1,27 @@
 package PIECES;
-
+import GAME.*;
+import ChessBoard.*;
 import ChessBoard.Board;
 import javax.swing.*;
 
 public abstract class Piece extends JButton{
-    Piece(){
+    public String image_Path ;
+    //    int loc_x, loc_y;
+    public String color;
+    JButton board[][];
+    Board main_Board = new Board();
+    Player white;
+    Player black;
+
+    Piece(Player w,Player b,JButton bd[][]){
+        white = w;
+        black = b;
+        board = bd;
 
 
     }
-    Piece(String path){
-        ImageIcon icon = createImageIcon(path);
-        //this.setBackground(color);
-        this.setIcon(icon);
-    }
+
+    public abstract String getColor();
 
     protected static ImageIcon createImageIcon(String path) {
         java.net.URL imgURL = Board.class.getResource(path);
@@ -22,6 +31,90 @@ public abstract class Piece extends JButton{
             System.err.println("Couldn't find file: " + path);
             return null;
         }
+    }
+
+    public abstract String getImage_Path();
+
+    public abstract boolean validateMove(int currentRow,int currentCol,int newRow , int newCol);
+
+    public boolean isAtPosition(int row,int col){
+
+        JButton pieceAtLocation = isEmpty(row,col);
+        //System.out.println(row+" "+col+" "+pieceAtLocation);
+        if(pieceAtLocation != null)
+            return true;
+        else
+            return false;
+
+    }
+
+    public Piece isEmpty(int r ,int c){
+
+
+        if(board[r][c] == null)
+            return null;
+        for(int i=0;i<8;i++){
+            if(board[r][c]==white.pawn[i]) {
+                return white.pawn[i];
+            }
+            else if(board[r][c]==black.pawn[i]) {
+                return black.pawn[i];
+            }
+        }
+        for(int i = 0 ;i<2 ;i++){
+            if(board[r][c] == white.rook[i]) {
+                return white.rook[i];
+            }
+            else if(board[r][c] == black.rook[i]) {
+                return black.rook[i];
+            }
+            else if(board[r][c] == white.bishop[i]) {
+                return white.bishop[i];
+            }
+            else if(board[r][c] == black.bishop[i]) {
+                return black.bishop[i];
+            }
+            else if(board[r][c] == white.knight[i]) {
+                return white.knight[i];
+            }
+            else if(board[r][c] == black.knight[i]) {
+                return black.knight[i];
+            }
+        }
+        if(board[r][c] == white.king)
+            return white.king;
+        else if(board[r][c] == black.king)
+            return black.king;
+        else if(board[r][c] == white.queen)
+            return white.queen;
+        else if(board[r][c] == black.queen)
+            return black.queen;
+        else
+            return null;
+    }
+
+    public boolean isInCheck(Player player){
+        King king = player.king;
+        int r =0,c=0;
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (board[row][col] == king) {
+                    r = row;
+                    c = col;
+                }
+            }
+        }
+        for(int x = 0; x<board.length; x++){
+            for(int y = 0; y<board[0].length; y++){
+                if(isAtPosition(x,y)){
+                    if(isEmpty(x,y).validateMove( x, y, r, c) && !isEmpty(x,y).getColor().equals(color)){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
 
